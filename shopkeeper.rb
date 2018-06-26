87a5f5ac6483132f3cbb8750c26b58194da2bdeb
Jason Swett <jason@benfranklinlabs.com>
2018-06-26 17:48:27 -0400

    Improve directory structure.

--- shopkeeper.rb
+++ shopkeeper.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env ruby
 
-require_relative 'config'
-require_relative 'product'
+require_relative 'config/database'
+require_relative 'models/product'
 
 Product.all.each do |p|
   puts p.name
