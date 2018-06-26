88f71302f611cd3fb4f06994015774b2e79f3af5
Jason Swett <jason@benfranklinlabs.com>
2018-06-26 18:35:05 -0400

    Add data.

--- db/setup.sql
+++ db/setup.sql
@@ -6,4 +6,10 @@ CREATE TABLE products (
   price_cents bigint NOT NULL
 );
 
-INSERT INTO products (name, price_cents) VALUES ('Fender Stratocaster', 999900), ('Gibson Les Paul', 499900);
+INSERT INTO products (name, price_cents)
+  VALUES ('Fender Stratocaster', 999900),
+         ('Fender Telecaster', 139999),
+         ('Gibson Les Paul', 499900),
+         ('Gibson SG', 73900),
+         ('Fender Standard Precision Bass', 59999),
+         ('Fender American Elite Jazz Bass', 199999);
