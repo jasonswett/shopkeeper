8731ced9d9fde69225536bf4bec9ec2b82dd5cd8
Jason Swett <jason@benfranklinlabs.com>
2018-07-20 09:35:48 -0400

    Update setup.sql.

--- db/setup.sql
+++ db/setup.sql
@@ -1,10 +1,33 @@
+DROP TABLE IF EXISTS line_items;
+DROP TABLE IF EXISTS sales;
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS customers;
 
 CREATE TABLE products (
   id bigserial PRIMARY KEY,
   name varchar(255) NOT NULL,
   price_cents bigint NOT NULL,
   CONSTRAINT name_unique UNIQUE(name)
+);
+
+CREATE TABLE customers (
+  id bigserial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+  CONSTRAINT email_unique UNIQUE(email)
+);
+
+CREATE TABLE sales (
+  id bigserial PRIMARY KEY,
+  customer_id bigint references customers(id) NOT NULL,
+  created_at timestamp NOT NULL
+);
+
+CREATE TABLE line_items (
+  id bigserial PRIMARY KEY,
+  sale_id bigint references sales(id) NOT NULL,
+  product_id bigint references products(id) NOT NULL,
+  quantity bigint NOT NULL
 );
 
 INSERT INTO products (name, price_cents)
